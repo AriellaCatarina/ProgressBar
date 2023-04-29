52fe87d4ed442e0ffd627eb2fb6c5b43039e3454
AriellaCatarina <132170758+AriellaCatarina@users.noreply.github.com>
2023-04-29 19:21:01 -0300

    Add files via upload

--- barra de progresso/main.lua
+++ barra de progresso/main.lua
@@ -1,3 +1,6 @@
+Fonte1 = love.graphics.newFont ("Bohemian Typewriter.ttf")
+--text = love.graphics.newText(Fonte1, "Esta barra de progresso leva 1 minuto para completar")
+
 function love.load()
    progress = 0 -- Progresso atual
    progressMax = 100 -- Progresso máximo
@@ -5,10 +8,13 @@ function love.load()
    barHeight = 20 -- Altura da barra
    lastTime = love.timer.getTime()
    progressCurrent = 0
-   progressSpeed = 10 -- valor de progresso a cada segundo
+   progressSpeed = 1 -- valor de progresso a cada segundo
 end
 
 function love.draw()
+   love.graphics.setColor(1, 1, 1)
+   love.graphics.print ("Esta barra de progresso leva \n1 minuto para completar", Fonte1, 100, 150, 0, 1.3) --love.graphics.print( text, x, y, r, sx, sy, ox, oy, kx, ky )
+   
    -- Desenha o fundo da barra
    love.graphics.setColor(1, 1, 1) -- Branco
    love.graphics.rectangle("fill", 100, 100, barWidth, barHeight)
@@ -16,6 +22,7 @@ function love.draw()
    -- Desenha o progresso atual
    love.graphics.setColor(0, 0.5, 1) -- Azul claro
   love.graphics.rectangle("fill", 100, 100, 200 * (progressCurrent / progressMax), 20) --love.graphics.rectangle( mode*, x, y, width, height, rx**, ry***, segments**** )
+
 end
 
 function love.update(dt)
@@ -42,4 +49,15 @@ end
 ****The number of segments used for drawing the round corners. A default amount will be chosen if no number is given.
 
 Ver: https://love2d.org/wiki/love.graphics.rectangle
+]]--
+
+--[[
+Estudo:
+Concatenação: 
+
+   variaveldetexto1 = "olá"
+   variaveldetexto2 = "mundo"
+
+   print (variaveldetexto1 + variaveldetexto2) = ERRADO!
+   print (variaveldetexto1 .. variaveldetexto2) = CERTO!
 ]]--
